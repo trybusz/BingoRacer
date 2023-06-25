7ae71507d13d94ebcbde063c27b63992ba8d765f
Trybusz <74030239+trybusz@users.noreply.github.com>
2023-06-24 20:04:48 -0700

    Basic Controller

---
+++ Assets/Scripts/Controllers/InputController.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+    public abstract class InputController : ScriptableObject {
+        public abstract float RetrieveMoveInput();
+        public abstract bool RetrieveJumpInput();
+    }
