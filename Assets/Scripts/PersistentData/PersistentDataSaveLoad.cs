eddc6fa1c7e8df2ad324ec15e4f69ea25532f606
philflip12 <pkeating105@cedarville.edu>
2023-07-11 20:56:24 -0400

    Attempt to improve getting Path to persistent data

--- Assets/Scripts/PersistentData/PersistentDataSaveLoad.cs
+++ Assets/Scripts/PersistentData/PersistentDataSaveLoad.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 
 public static class PersistentDataSaveLoad
 {
+    static string persistentDataPath;
+
+    static PersistentDataSaveLoad() {
+        // TODO: change this to a static function which is called in the
+        // "Awake" or "Start" function of some other class which will only
+        // be called once and will be called prior to the need to load or
+        // save game state.
+        persistentDataPath = Application.persistentDataPath;
+    }
+
     public static bool WriteToFile(string fileName, string fileData)
     {
-        var fullPath = Path.Combine(Application.persistentDataPath, fileName);
+        var fullPath = Path.Combine(persistentDataPath, fileName);
 
         try
         {
@@ -23,7 +33,7 @@ public static class PersistentDataSaveLoad
 
     public static bool LoadFromFile(string fileName, out string fileData)
     {
-        var fullPath = Path.Combine(Application.persistentDataPath, fileName);
+        var fullPath = Path.Combine(persistentDataPath, fileName);
 
         try
         {
@@ -39,7 +49,7 @@ public static class PersistentDataSaveLoad
     }
 
     public static bool FileExists(string fileName) {
-        var fullPath = Path.Combine(Application.persistentDataPath, fileName);
+        var fullPath = Path.Combine(persistentDataPath, fileName);
         return File.Exists(fullPath);
     }
 }
