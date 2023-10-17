c187efec1b45b63bf9d0560e5af70b7f200975a8
philflip12 <pkeating105@cedarville.edu>
2023-10-17 00:27:59 -0400

    Got lobby working!!

--- Assets/Scripts/MenuNavigation/HostGameNavigationScript.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class JoinGameNavigationScript : MonoBehaviour
-{
-    public void backToMultiplayerMenu() {
-        SceneManager.LoadScene("MultiplayerMenu");
-    }
-}
--- Assets/Scripts/Multiplayer/MultiplayerManagerScript.cs
+++ Assets/Scripts/Multiplayer/MultiplayerManagerScript.cs
@@ -13,16 +13,21 @@ using System;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine.UI;
-using System.Linq;
+using UnityEngine.SceneManagement;
 
 public class MultiplayerManagerScript : MonoBehaviour {
     public static MultiplayerManagerScript instance { get; private set; }
 
     // constants
-    private const float hostLobbyHeartbeatPeriod = 20f;
-    private const float listLobbiesCooldown = 1.1f;
-    private const float updateLobbyPeriod = 1.1f;
-    private const float updatePlayerCooldown = 1.1f;
+    private const float HOST_LOBBY_HEARTBEAT_PERIOD = 20f;
+    private const float LIST_LOBBIES_COOLDOWN = 1.1f;
+    private const float UPDATE_LOBBY_PERIOD = 1.1f;
+    private const float UPDATE_PLAYER_COOLDOWN = 1.1f;
+    private const string PLAYER_NAME_KEY = "Name";
+    private const string PLAYER_TEAM_KEY = "Team";
+    private const string PLAYER_READY_KEY = "Ready";
+    private const string LOBBY_JOIN_CODE_KEY = "JoinCode";
+    private const string LOBBY_NO_JOIN_CODE = "";
 
     // UI objects
     public GameObject playerNameInput;
@@ -42,6 +47,7 @@ public class MultiplayerManagerScript : MonoBehaviour {
     private Lobby lobby = null;
     private bool isHost = false;
     private bool inLobby = false;
+    private bool inGame = false;
     private string joinCode = "NotSet";
     private float hostLobbyHeartbeatTimer = 0f;
     private float listLobbiesTimer = 0f;
@@ -67,10 +73,9 @@ public class MultiplayerManagerScript : MonoBehaviour {
             await AuthenticationService.Instance.SignInAnonymouslyAsync();
 
             player = new Player(AuthenticationService.Instance.PlayerId, null, new(3));
-            player.Data["Name"] = new(PlayerDataObject.VisibilityOptions.Member, "NoName");
-            player.Data["Team"] = new(PlayerDataObject.VisibilityOptions.Member, "Red");
-            player.Data["Ready"] = new(PlayerDataObject.VisibilityOptions.Member, "false");
-            //player.Data.Add("ready", new(PlayerDataObject.VisibilityOptions.Member, "false"));
+            player.Data[PLAYER_NAME_KEY] = new(PlayerDataObject.VisibilityOptions.Member, "NoName");
+            player.Data[PLAYER_TEAM_KEY] = new(PlayerDataObject.VisibilityOptions.Member, "Red");
+            player.Data[PLAYER_READY_KEY] = new(PlayerDataObject.VisibilityOptions.Member, "false");
 
             DontDestroyOnLoad(gameObject);
         } catch (Exception e) {
@@ -79,9 +84,11 @@ public class MultiplayerManagerScript : MonoBehaviour {
     }
 
     private void Update() {
-        HostLobbyHeartBeat();
-        GetLobbyUpdate();
-        UpdatePlayer();
+        if (!inGame) {
+            HostLobbyHeartBeat();
+            UpdatePlayer();
+            GetLobbyUpdate();
+        }
     }
 
     public void CleanUp() {
@@ -100,6 +107,7 @@ public class MultiplayerManagerScript : MonoBehaviour {
         }
 
         Destroy(gameObject);
+        instance = null;
     }
 
     public async void CreateLobby() {
@@ -112,11 +120,14 @@ public class MultiplayerManagerScript : MonoBehaviour {
             int maxPlayers = 2;
             CreateLobbyOptions options = new() {
                 IsPrivate = false,
-                Player = player
+                Player = player,
+                Data = new Dictionary<string, DataObject> {
+                    {LOBBY_JOIN_CODE_KEY, new DataObject(DataObject.VisibilityOptions.Member, LOBBY_NO_JOIN_CODE)}
+                }
             };
             lobby = await LobbyService.Instance.CreateLobbyAsync(lobbyName, maxPlayers, options);
 
-            hostLobbyHeartbeatTimer = Time.time + hostLobbyHeartbeatPeriod;
+            hostLobbyHeartbeatTimer = Time.time + HOST_LOBBY_HEARTBEAT_PERIOD;
             isHost = true;
             inLobby = true;
             joinCodeInput.GetComponent<TMP_InputField>().text = lobby.LobbyCode;
@@ -129,7 +140,7 @@ public class MultiplayerManagerScript : MonoBehaviour {
 
     private async void HostLobbyHeartBeat() {
         if (!inLobby || !isHost || Time.time < hostLobbyHeartbeatTimer) return;
-        hostLobbyHeartbeatTimer = Time.time + hostLobbyHeartbeatPeriod;
+        hostLobbyHeartbeatTimer = Time.time + HOST_LOBBY_HEARTBEAT_PERIOD;
 
         try {
             await LobbyService.Instance.SendHeartbeatPingAsync(lobby.Id);
@@ -140,10 +151,15 @@ public class MultiplayerManagerScript : MonoBehaviour {
 
     private async void GetLobbyUpdate() {
         if (!inLobby || Time.time < updateLobbyTimer) return;
-        updateLobbyTimer = Time.time + updateLobbyPeriod;
+        updateLobbyTimer = Time.time + UPDATE_LOBBY_PERIOD;
 
         try {
             lobby = await LobbyService.Instance.GetLobbyAsync(lobby.Id);
+
+            if (!isHost && lobby.Data[LOBBY_JOIN_CODE_KEY].Value != LOBBY_NO_JOIN_CODE) {
+                JoinGame();
+                return;
+            }
 
             for (int i = 0; i < playerListHolder.transform.childCount; i++) {
                 Destroy(playerListHolder.transform.GetChild(i).gameObject);
@@ -156,9 +172,9 @@ public class MultiplayerManagerScript : MonoBehaviour {
                 Transform playerName = currEntry.transform.GetChild(0);
                 Transform team = currEntry.transform.GetChild(1);
                 Transform ready = currEntry.transform.GetChild(2);
-                playerName.GetComponent<TextMeshProUGUI>().SetText(playerEntry.Data["Name"].Value);
-                team.GetComponent<TextMeshProUGUI>().SetText(playerEntry.Data["Team"].Value);
-                if (playerEntry.Data["Ready"].Value == "true") {
+                playerName.GetComponent<TextMeshProUGUI>().SetText(playerEntry.Data[PLAYER_NAME_KEY].Value);
+                team.GetComponent<TextMeshProUGUI>().SetText(playerEntry.Data[PLAYER_TEAM_KEY].Value);
+                if (playerEntry.Data[PLAYER_READY_KEY].Value == "true") {
                     ready.GetComponent<TextMeshProUGUI>().SetText("Yep!");
                 } else {
                     ready.GetComponent<TextMeshProUGUI>().SetText("Nope");
@@ -173,7 +189,7 @@ public class MultiplayerManagerScript : MonoBehaviour {
     public async void ListLobbies() {
         if (inLobby) return;
         if (Time.time < listLobbiesTimer) return;
-        listLobbiesTimer = Time.time + listLobbiesCooldown;
+        listLobbiesTimer = Time.time + LIST_LOBBIES_COOLDOWN;
 
         try {
             QueryResponse queryResponse = await Lobbies.Instance.QueryLobbiesAsync();
@@ -232,12 +248,12 @@ public class MultiplayerManagerScript : MonoBehaviour {
     public void ToggleReady() {
         if (!inLobby) return;
 
-        if (player.Data["Ready"].Value == "true") {
+        if (player.Data[PLAYER_READY_KEY].Value == "true") {
             readyButton.GetComponent<Image>().color = new Color(255f/255f, 239f/255f, 180f/255f);
-            player.Data["Ready"].Value = "false";
+            player.Data[PLAYER_READY_KEY].Value = "false";
         } else {
             readyButton.GetComponent<Image>().color = new Color(132f/255f, 222f/255f, 2f/255f);
-            player.Data["Ready"].Value = "true";
+            player.Data[PLAYER_READY_KEY].Value = "true";
         }
 
         playerUpdateNeeded = true;
@@ -246,27 +262,63 @@ public class MultiplayerManagerScript : MonoBehaviour {
     public void ToggleTeam() {
         if (!inLobby) return;
 
-        if (player.Data["Team"].Value == "Red") {
+        if (player.Data[PLAYER_TEAM_KEY].Value == "Red") {
             teamButton.GetComponent<Image>().color = new Color(98f/255f, 161f/255f, 221f/255f);
-            player.Data["Team"].Value = "Blue";
+            player.Data[PLAYER_TEAM_KEY].Value = "Blue";
         } else {
             teamButton.GetComponent<Image>().color = new Color(222f/255f, 97f/255f, 100f/255f);
-            player.Data["Team"].Value = "Red";
+            player.Data[PLAYER_TEAM_KEY].Value = "Red";
         }
 
         playerUpdateNeeded = true;
     }
 
-    public void StartGame() {
+    public async void StartGame() {
         if (!inLobby || !isHost) return;
 
         foreach (Player playerEntry in lobby.Players) {
-            if (playerEntry.Data["Ready"].Value != "true") {
+            if (playerEntry.Data[PLAYER_READY_KEY].Value != "true") {
                 return;
             }
         }
 
-        Debug.Log("Starting Game!");
+        try {
+            Allocation allocation = await RelayService.Instance.CreateAllocationAsync(lobby.MaxPlayers - 1);
+            string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+            RelayServerData relayServerData = new RelayServerData(allocation, "dtls");
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+            NetworkManager.Singleton.StartHost();
+
+            UpdateLobbyOptions options = new() {
+                IsPrivate = lobby.IsPrivate,
+                Data = lobby.Data
+            };
+            options.Data[LOBBY_JOIN_CODE_KEY] = new DataObject(DataObject.VisibilityOptions.Member, joinCode);
+            await LobbyService.Instance.UpdateLobbyAsync(lobby.Id, options);
+
+            inGame = true;
+
+            NetworkManager.Singleton.SceneManager.LoadScene("BingoBoardMenu", LoadSceneMode.Single);
+        } catch (RelayServiceException e) {
+            Debug.Log(e);
+        } catch (LobbyServiceException e) {
+            Debug.Log(e);
+        }
+    }
+
+    private async void JoinGame() {
+        if (!inLobby || isHost) return;
+        try {
+            string joinCode = lobby.Data[LOBBY_JOIN_CODE_KEY].Value;
+            JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
+            RelayServerData relayServerData = new RelayServerData(joinAllocation, "dtls");
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+            NetworkManager.Singleton.StartClient();
+
+            inGame = true;
+        } catch (RelayServiceException e) {
+            Debug.Log(e);
+        }
     }
 
     private void SetPlayerName() {
@@ -274,12 +326,12 @@ public class MultiplayerManagerScript : MonoBehaviour {
         if (name == "") {
             name = "Anonymous";
         }
-        player.Data["Name"].Value = name;
+        player.Data[PLAYER_NAME_KEY].Value = name;
     }
 
     private void UpdatePlayer() {
         if (!inLobby || !playerUpdateNeeded || Time.time < updatePlayerTimer) return;
-        updatePlayerTimer = Time.time + updatePlayerCooldown;
+        updatePlayerTimer = Time.time + UPDATE_PLAYER_COOLDOWN;
 
         UpdatePlayerOptions options = new() {
             Data = player.Data
