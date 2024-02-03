b8d26ce65677a2df6791549653d43274587df3b1
philflip12 <pkeating105@cedarville.edu>
2024-02-03 01:39:13 -0500

    fix backgrounds on zachs levels

--- Assets/Scripts/PersistentData/LevelTimesData.cs
+++ Assets/Scripts/PersistentData/LevelTimesData.cs
@@ -9,7 +9,7 @@ public class LevelTimesData {
     public List<LevelFolder> levelFolders;
     
     public LevelTimesData() {
-        this.GetLevelTimeData();
+        GetLevelTimeData();
     }
 
     public int UpdateLevelTime(string folderName, string levelName, float time) {
@@ -94,12 +94,11 @@ public class LevelTimesData {
 
     private void GetLevelTimeData() {
         if (PersistentDataSaveLoad.FileExists("LevelTimesData")) {
-            string levelTimeDataJson = "";
-            PersistentDataSaveLoad.LoadFromFile("LevelTimesData", out levelTimeDataJson); 
+            PersistentDataSaveLoad.LoadFromFile("LevelTimesData", out string levelTimeDataJson);
             LoadFromJson(levelTimeDataJson);
         }
         else {
-            this.InitLevelData();
+            InitLevelData();
         }
     }
 
@@ -111,38 +110,38 @@ public class LevelTimesData {
         JsonUtility.FromJsonOverwrite(a_Json, this);
     }
 
-    private void InitLevelData() {
-        // Zach is the worst for making me hard code this :)
-        
-        List<LevelTime> Folder1_levelTimes = new();
-        Folder1_levelTimes.Add(new LevelTime("OGLevel1"));
-        Folder1_levelTimes.Add(new LevelTime("OGLevel2"));
-        Folder1_levelTimes.Add(new LevelTime("OGLevel3"));
-        Folder1_levelTimes.Add(new LevelTime("OGLevel4"));
-        Folder1_levelTimes.Add(new LevelTime("OGLevel5"));
-        Folder1_levelTimes.Add(new LevelTime("OGLevel6"));
-        Folder1_levelTimes.Add(new LevelTime("OGLevel7"));
-        Folder1_levelTimes.Add(new LevelTime("OGLevel8"));
-        Folder1_levelTimes.Add(new LevelTime("OGLevel9"));
-        Folder1_levelTimes.Add(new LevelTime("OGLevel10"));
-        Folder1_levelTimes.Add(new LevelTime("OGLevel11"));
-        Folder1_levelTimes.Add(new LevelTime("OGLevel12"));
-        Folder1_levelTimes.Add(new LevelTime("OGLevel13"));
-        Folder1_levelTimes.Add(new LevelTime("OGLevel14"));
-        Folder1_levelTimes.Add(new LevelTime("OGLevel15"));
-        Folder1_levelTimes.Add(new LevelTime("OGLevel16"));
-        Folder1_levelTimes.Add(new LevelTime("OGLevel17"));
-        Folder1_levelTimes.Add(new LevelTime("OGLevel18"));
-        Folder1_levelTimes.Add(new LevelTime("OGLevel19"));
-        Folder1_levelTimes.Add(new LevelTime("OGLevel20"));
-        Folder1_levelTimes.Add(new LevelTime("OGLevel21"));
-        Folder1_levelTimes.Add(new LevelTime("OGLevel22"));
-        Folder1_levelTimes.Add(new LevelTime("OGLevel23"));
-        Folder1_levelTimes.Add(new LevelTime("OGLevel24"));
-        Folder1_levelTimes.Add(new LevelTime("OGLevel25"));
+    private void InitLevelData() {        
+        List<LevelTime> Folder1_levelTimes = new(){
+            new LevelTime("OGLevel1"),
+            new LevelTime("OGLevel2"),
+            new LevelTime("OGLevel3"),
+            new LevelTime("OGLevel4"),
+            new LevelTime("OGLevel5"),
+            new LevelTime("OGLevel6"),
+            new LevelTime("OGLevel7"),
+            new LevelTime("OGLevel8"),
+            new LevelTime("OGLevel9"),
+            new LevelTime("OGLevel10"),
+            new LevelTime("OGLevel11"),
+            new LevelTime("OGLevel12"),
+            new LevelTime("OGLevel13"),
+            new LevelTime("OGLevel14"),
+            new LevelTime("OGLevel15"),
+            new LevelTime("OGLevel16"),
+            new LevelTime("OGLevel17"),
+            new LevelTime("OGLevel18"),
+            new LevelTime("OGLevel19"),
+            new LevelTime("OGLevel20"),
+            new LevelTime("OGLevel21"),
+            new LevelTime("OGLevel22"),
+            new LevelTime("OGLevel23"),
+            new LevelTime("OGLevel24"),
+            new LevelTime("OGLevel25")
+        };
 
-        List<LevelFolder> levelFolders = new();
-        levelFolders.Add(new LevelFolder("OGLevelSelect", Folder1_levelTimes));
+        List<LevelFolder> levelFolders = new(){
+            new LevelFolder("OGLevelSelect", Folder1_levelTimes)
+        };
 
         this.levelFolders = levelFolders;
     }
